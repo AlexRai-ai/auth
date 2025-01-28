004d03f1fa0e3144d97bb3a06c87d417f41eba3b
AlexRai-ai <nikhil@skyaitechnologies.com>
2025-01-28 11:01:53 +0530

    Update settings.py
    hiding of secrets

--- main/main/settings.py
+++ main/main/settings.py
@@ -116,8 +116,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nikhilrai662@gmail.com'
-EMAIL_HOST_PASSWORD = 'igza mkjr zuel czcv'
+EMAIL_HOST_USER = 'your_email_add'
+EMAIL_HOST_PASSWORD = 'your_app_password'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
